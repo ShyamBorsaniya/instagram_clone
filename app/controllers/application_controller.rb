9acc2ccd05416cb603b8ded6caa7dc0c4dc4febb
ShyamBorsaniya <borsaniyashyam2772@gmail.com>
2024-03-07 16:30:24 +0530

    Add sign_in-with-username-&-email-both

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,17 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protect_from_forgery with: :null_session
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :full_name])
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :full_name])
+
+    added_attrs = [:username, :full_name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
 end
